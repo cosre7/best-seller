b9a6abbb1cc5aa94ac96abccf256b17a090df1b4
cosre7 <cosre7@gmail.com>
2024-04-01 01:58:15 +0900

    [refactor] key 추가

--- app/list/[name]/page.tsx
+++ app/list/[name]/page.tsx
@@ -1,21 +1,31 @@
 import { API_URL } from "../../constants"
 
+interface IParmas {
+    params: {
+        name: string
+    }
+}
 async function getBooks(name:string) {
     const response = await fetch(`${API_URL}/list?name=${name}`)
     return response.json()
 }
 
-export default async function listPage({params: {name}} : {params: {name: string}}) {
+export async function generateMetadata({params: {name}} : IParmas) {
+    return {
+        title: name
+    }
+}
+
+export default async function listPage({params: {name}} : IParmas) {
     const bookList = await getBooks(name)
-    console.log(bookList)
     return (
         <div>
             {bookList.results.books.map(book => 
-                (<div>
+                (<div key={book.primary_isbn10}>
                     <img src={book.book_image} />
                     <h2>{book.title}</h2>
                     <h2>{book.author}</h2>
-                    <a href={book.buy_links[0].url}>Buy Now</a>
+                    <a href={book.buy_links[0].url} target={"_blank"}>Buy Now</a>
                 </div>)
             )}
         </div>
